c85d17ad92a23079faa9e5c3ed93ab77859ef65c
João Arthur <tupi_1800@hotmail.com>
2026-03-18 20:56:40 -0300

    Inicializa banco com um usuario

---
+++ app/src/db/seed.ts
@@ -0,0 +1,19 @@
+import { db } from "../index";
+import { usersTable } from "./schema";
+import bcrypt from "bcryptjs";
+
+async function seed() {
+  console.log("🌱 Populando banco de dados para testes...");
+  const hashedPassword = await bcrypt.hash("123", 10);
+  
+  await db.insert(usersTable).values({
+    name: "Nome de Teste",
+    email: "e2e@email.com",
+    password: hashedPassword,
+  }).onConflictDoNothing();
+
+  console.log("✅ Usuário de teste criado!");
+  process.exit(0);
+}
+
+seed();
